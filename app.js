53261e89a0d5fcbbf10441afe04b47212aadbe7f
Manh Tri <mtri02.it@gmail.com>
2022-06-29 18:07:58 +0700

    feat: Implemented user login

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const port = 3003;
 const middleware = require("./middleware");
 const path = require("path");
 const database = require("./db/database");
+const session = require("express-session");
 
 const server = app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
@@ -15,6 +16,14 @@ app.set("views", "views");
 app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
+app.use(
+  session({
+    secret: "bbq chips",
+    resave: true,
+    saveUninitialized: false,
+  })
+);
+
 // Routes
 const loginRoute = require("./routes/loginRoutes");
 const registerRoute = require("./routes/registerRoutes");
@@ -25,6 +34,7 @@ app.use("/register", registerRoute);
 app.get("/", middleware.requireLogin, (req, res, next) => {
   var payload = {
     pageTitle: "Home",
+    userLoggedIn: req.session.user,
   };
 
   res.status(200).render("home", payload);
